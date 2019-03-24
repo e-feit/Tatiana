675422ce2e699cdaabfd3a568296177a3f1baac6
Eugen Feit <eugen.feit@gmail.com>
2019-03-24 21:01:21 +0100

    Add SQLAlchemy and user model

--- frontend-flask/app/__init__.py
+++ frontend-flask/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from app.db.models import db
 from .home.route import blueprint as home_blueprint
 from .page_2.route import blueprint as page_2_blueprint
 
@@ -7,7 +8,14 @@ def create_app():
     app = Flask(__name__)
     app.static_folder = 'assets'
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@127.0.0.1/tatiana?charset=utf8'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Здесь должны быть зарегистрированы все blueprints.
+    # При создании нового, не забываем указать его здесь.
     app.register_blueprint(home_blueprint)
     app.register_blueprint(page_2_blueprint, url_prefix="/page-2")
+
+    db.init_app(app)
 
     return app
---
+++ frontend-flask/app/db/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(session_options={'autocommit': True})
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(10), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(120), nullable=True)
+    last_login = db.Column(db.DateTime(), nullable=True)
