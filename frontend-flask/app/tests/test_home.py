7b67469cda17c6eba603bdb8008cdcda8e51e610
Eugen Feit <eugen.feit@gmail.com>
2019-03-24 10:57:22 +0100

    Refactored tests -> moved to module folder

--- frontend-flask/app/tests/test_home.py
+++
@@ -1,6 +0,0 @@
-def test_hello_world(client):
-    """Hello world message on root access."""
-
-    response = client.get('/')
-    assert b'Hello, World!' in response.data
-    assert 200 == response.status_code
